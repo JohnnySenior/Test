f255b41fee6382f7c6708af0c6aa0cb36a124fc5
JohnnySenior <jamshidbektursunboev@gmail.com>
2023-07-31 20:27:55 +0900

    Calculate-Sum

--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
-﻿Console.WriteLine("Hello, Github!");
-Console.WriteLine("Hello, Johnny!");
-Console.WriteLine("Hello, Siroj!");
+﻿Console.WriteLine();
+Console.WriteLine("This console application calculates 'Sum of Array'");
+Console.WriteLine();
+
+Console.Write("Enter the size of Array: ");
+int sizeOfArray = int.Parse(Console.ReadLine());
+int sumOfArray = 0;
+
+int[] myArray = new int[sizeOfArray];
+
+for(int iteration = 0; iteration < sizeOfArray; iteration++)
+{
+    Console.Write($"myArray[{iteration}] = ");
+    myArray[iteration] = int.Parse(Console.ReadLine());
+}
+
+Console.Write("Elements of myArray: ");
+
+for(int iteration = 0; iteration < sizeOfArray; iteration++)
+{
+    Console.Write($"{myArray[iteration]} ");
+}
+
+Console.WriteLine();
+
+for(int iteration = 0; iteration < sizeOfArray; iteration++)
+{
+    sumOfArray += myArray[iteration];
+}
+
+Console.WriteLine($"sumOfArray = {sumOfArray}");
